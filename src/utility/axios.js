eefe0c92763e36a52afb79ded8b7f3c1c9dfb63b
tihitina-k <tihitinak19@gmail.com>
2025-11-01 16:49:32 +0300

    frontend update

--- src/utility/axios.js
+++ src/utility/axios.js
@@ -1,8 +1,3 @@
-// src/utility/axios.js
-import axios from "axios";
-
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5173/api/v1", // backend URL
+  baseURL: "https://evangadibackend.onrender.com/api/v1",
 });
-
-
